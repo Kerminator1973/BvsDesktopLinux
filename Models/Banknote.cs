45ffeb4f769b791cca890aafd7e7b45a35d35426
Maksim Rozhkov <kermit@mail.ru>
2022-04-23 20:10:36 +0300

    Initial Commit

---
+++ Models/Banknote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BvsDesktopLinux.Models
+{
+    public class Banknote
+    {
+        public string Currency { get; set; }
+
+        public string Denomination { get; set; }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using BvsDesktopLinux.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BvsDesktopLinux.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Banknote> Banknotes { get; }
+
+        public MainWindowViewModel()
+        {
+            Banknotes = new ObservableCollection<Banknote>(GenerateMockBanknotesTable());
+        }
+
+        private IEnumerable<Banknote> GenerateMockBanknotesTable()
+        {
+            var defaultBanknotes = new List<Banknote>()
+            {
+                new Banknote()
+                {
+                    Currency = "RUB",
+                    Denomination = "10"
+                },
+                new Banknote()
+                {
+                    Currency = "RUB",
+                    Denomination = "50"
+                },
+                new Banknote()
+                {
+                    Currency = "RUB",
+                    Denomination = "100"
+                }
+            };
+
+            return defaultBanknotes;
+        }
+    }
+}
