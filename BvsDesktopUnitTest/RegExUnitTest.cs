253df1b0c9790401f03c906674264a73e5d91bc5
Maksim Rozhkov <kermit@mail.ru>
2022-05-29 14:10:02 +0300

    Разбор анализа RegEx шаблона

--- BvsDesktopUnitTest/RegExUnitTest.cs
+++ BvsDesktopUnitTest/RegExUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace BvsDesktopTests
@@ -6,6 +7,8 @@ namespace BvsDesktopTests
     [TestClass]
     public class RegExUnitTest
     {
+        // Пример части HTML-документа генерируемым прибором D820F.
+        // https://github.com/Kerminator1973/BVSDesktopSupport/blob/main/Examples/DORS-8xx.html
         private readonly string htmlExample = new(@"
             <tr valign=""middle"">
               < td align = ""center"" width = ""60%"" colspan = ""2"" >
@@ -25,16 +28,40 @@ namespace BvsDesktopTests
                 </form>
               </td>
               <td align = ""left"" class=""CSS_MONO_FONT"" colspan=""2"">
-                <a href = ""http://192.168.1.254/ser_num/00001322.zip"" >/ ser_num / 00001322.zip </ a >
+                <a href = 'http://192.168.1.254/ser_num/00001322.zip' >/ ser_num / 00001322.zip </ a >
                 < br > 394 bytes
               </td>
             </tr>
         ");
 
+        // Шаблон для извлечения ссылок на пересчёты купюр
+
+        // Расшифровка старого варианта:
+        //private readonly string reHrefPattern = new(@"(?inx)  // Flags: insensitive, no capture, extended
+        //                <a \s [^>]*                           // "<a" потом пробел и не ">": т.е. начало тэга <a>
+        //                    href \s* = \s*                    // должен быть атрибут "href"
+        //                        (?<q> ['""] )                 // Дальше может быть символ кавычка, либо двойная кавычка;
+        //                                                          это была Capture Group с именем "q" (quote)
+        //                            (?<url> [^""]+ )          // Это Capture Group, которая завершается двойной кавычкой
+        //                        \k<q>
+        //                [^>]* >");                            // Строка должна завершаться завершением тэга
+
+        private readonly string reHrefPattern = new(@"(?inx)
+                        <a\s[^>]*
+                            href\s*=\s*
+                                (?<q>['""])
+                                    (?<url>[^'""]+)
+                                \k<q>
+                        [^>]*>");
+
+        // Шаблон для извлечения идентификатора устройства
         private readonly string htmlUinHeader = new(@"
             <p style=""text-align: center; margin: 0px; padding: 10px; font: bold 20px serif"">UIN: D820-001-00000775</p>
         ");
 
+        private readonly string reUinPattern = new(@">UIN:.*?<\/p>");
+
+        // Тест извлечения УИН-а
         private void matchUIN(Regex reUIN)
         {
             Match m = reUIN.Match(htmlUinHeader);
@@ -48,8 +75,8 @@ namespace BvsDesktopTests
         [TestMethod]
         public void TestRegExSimple()
         {
-            var reUIN = new Regex(@">UIN:.*?<\/p>");
-            for(int i = 0; i < 100000; i++)
+            var reUIN = new Regex(reUinPattern);
+            for (int i = 0; i < 100000; i++)
             {
                 matchUIN(reUIN);
             }
@@ -58,11 +85,27 @@ namespace BvsDesktopTests
         [TestMethod]
         public void TestRegExSimpleCompiled()
         {
-            var reUIN = new Regex(@">UIN:.*?<\/p>", RegexOptions.Compiled);
+            var reUIN = new Regex(reUinPattern, RegexOptions.Compiled);
             for (int i = 0; i < 100000; i++)
             {
                 matchUIN(reUIN);
             }
+        }
+
+        [TestMethod]
+        public void TestRegExComplexCompiled()
+        {
+            var reTemplate = new Regex(reHrefPattern, RegexOptions.Compiled);
+
+            List<string> urls = new();
+            foreach (Match match in reTemplate.Matches(htmlExample))
+            {
+                urls.Add(match.Groups["url"].ToString());
+            }
+
+            Assert.IsTrue(urls.Count == 2);
+            Assert.IsTrue(urls[0] == "http://192.168.1.254/ser_num/00001321.zip");
+            Assert.IsTrue(urls[1] == "http://192.168.1.254/ser_num/00001322.zip");
         }
     }
 }
